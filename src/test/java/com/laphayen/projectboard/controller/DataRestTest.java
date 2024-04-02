e3779290b642eddf231c923aac333b036281f7de
Baek Gi Chan <laphayen@gmail.com>
2024-04-02 22:28:27 +0900

    #1 - Spring Data REST 테스트 제외 처리
    The integration tests are heavy and affect the database as they are implemented using Data REST functionality rather than business logic of the project.
    
    Therefore, it should be excluded from the overall tests execution.

--- src/test/java/com/laphayen/projectboard/controller/DataRestTest.java
+++ src/test/java/com/laphayen/projectboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.laphayen.projectboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -15,6 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data REST 통합테스트는 불필요하므로 제외")
 @DisplayName("Data REST - API 테스트")
 @Transactional
 @AutoConfigureMockMvc
