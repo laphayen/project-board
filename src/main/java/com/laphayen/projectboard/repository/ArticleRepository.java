8b8c4565af5c24f5f72b8fc555dfde9259ff7a0e
Baek Gi Chan <laphayen@gmail.com>
2024-04-02 22:05:30 +0900

    #1 - Data REST configuration
    Configure to automatically make the JSON API for posts and comments RESTful.

--- src/main/java/com/laphayen/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/laphayen/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.laphayen.projectboard.repository;
 
 import com.laphayen.projectboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
