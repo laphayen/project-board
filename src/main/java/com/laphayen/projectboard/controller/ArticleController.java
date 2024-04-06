07f6a925136c61e1173b12fba573a727eb759499
Baek Gi Chan <laphayen@gmail.com>
2024-04-06 21:48:22 +0900

    #16 - Implement the basic structure of the article view
    Implement controller, view, and tests.

--- src/main/java/com/laphayen/projectboard/controller/ArticleController.java
+++ src/main/java/com/laphayen/projectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.laphayen.projectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,5 +16,12 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articlesId}")
+    public String article(@PathVariable Long articlesId, ModelMap map) {
+        map.addAttribute("article", "article"); // TODO: 구현 시 실제 데이터 삽입
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 }
