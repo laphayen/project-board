f8faf9a428b5c25f1b5fb018830edf76ec2e3b45
Baek Gi Chan <laphayen@gmail.com>
2024-04-11 23:14:20 +0900

    #20 - Implement the structure and tests for the comment service logic
    Implement simple CRUD functionality related to comments through testing.

---
+++ src/main/java/com/laphayen/projectboard/service/ArticleCommentService.java
@@ -0,0 +1,27 @@
+package com.laphayen.projectboard.service;
+
+import com.laphayen.projectboard.dto.ArticleCommentDto;
+import com.laphayen.projectboard.repository.ArticleCommentRepository;
+import com.laphayen.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleCommentService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleCommentRepository articleCommentRepository;
+
+    @Transactional(readOnly = true)
+    public List<ArticleCommentDto> searchArticleComment(Long articleId) {
+        return List.of();
+    }
+
+    public void saveArticleComment(ArticleCommentDto dto) {
+    }
+}
