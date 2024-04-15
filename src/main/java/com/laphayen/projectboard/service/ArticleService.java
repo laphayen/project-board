9d30023a784336819eaef9cf1c78be1a0662320e
Baek Gi Chan <laphayen@gmail.com>
2024-04-15 23:50:50 +0900

    #20 - Apply changes to DTOs in the services for Article and Comment
    Apply changes to DTOs in the services for Article and Comment.

--- src/main/java/com/laphayen/projectboard/service/ArticleService.java
+++ src/main/java/com/laphayen/projectboard/service/ArticleService.java
@@ -2,14 +2,13 @@ package com.laphayen.projectboard.service;
 
 import com.laphayen.projectboard.domain.type.SearchType;
 import com.laphayen.projectboard.dto.ArticleDto;
-import com.laphayen.projectboard.dto.ArticleUpdateDto;
+import com.laphayen.projectboard.dto.ArticleWithCommentsDto;
 import com.laphayen.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @RequiredArgsConstructor
 @Transactional
@@ -19,21 +18,22 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long l) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
     }
+
 }
