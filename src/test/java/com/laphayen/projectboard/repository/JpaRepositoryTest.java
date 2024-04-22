b677519518d0b24ead702cec3d6986c769d83e67
Baek Gi Chan <laphayen@gmail.com>
2024-04-22 22:57:54 +0900

    #31 - Modify the test for adding a new account with the applied UK
    Modify the 'userId' name in the 'data.sql' test data to avoid duplication caused by applying the unique key.

--- src/test/java/com/laphayen/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/laphayen/projectboard/repository/JpaRepositoryTest.java
@@ -52,7 +52,7 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         // Given
         long previousCount = articleRepository.count();
-        UserAccount userAccount = userAccountRepository.save(UserAccount.of("lapahyen", "qwer1234", null, null, null));
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("newlaphayen", "pw", null, null, null));
         Article article = Article.of(userAccount, "new article", "new content", "#spring");
 
         // When
