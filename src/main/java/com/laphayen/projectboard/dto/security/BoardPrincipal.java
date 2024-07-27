0c83782d7e8ebb09a0f0874640f91a3392c05ae1
Baek Gi Chan <laphayen@gmail.com>
2024-07-27 14:49:41 +0900

    #97 - Add functionality to handle OAuth authentication information
    Implement the OAuth2User interface so that the Spring OAuth Client recognizes authentication information in the same way as before.

--- src/main/java/com/laphayen/projectboard/dto/security/BoardPrincipal.java
+++ src/main/java/com/laphayen/projectboard/dto/security/BoardPrincipal.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,10 +18,15 @@ public record BoardPrincipal(
         Collection<? extends GrantedAuthority> authorities,
         String email,
         String nickname,
-        String memo
-) implements UserDetails {
+        String memo,
+        Map<String, Object> oAuth2Attributes
+) implements UserDetails, OAuth2User {
 
     public static BoardPrincipal of(String username, String password, String email, String nickname, String memo) {
+        return of(username, password, email, nickname, memo, Map.of());
+    }
+
+    public static BoardPrincipal of(String username, String password, String email, String nickname, String memo, Map<String, Object> oAuth2Attributes) {
         Set<RoleType> roleTypes = Set.of(RoleType.USER);
 
         return new BoardPrincipal(
@@ -32,7 +39,8 @@ public record BoardPrincipal(
                 ,
                 email,
                 nickname,
-                memo
+                memo,
+                oAuth2Attributes
         );
     }
 
@@ -65,6 +73,8 @@ public record BoardPrincipal(
     @Override public boolean isCredentialsNonExpired() { return true; }
     @Override public boolean isEnabled() { return true; }
 
+    @Override public Map<String, Object> getAttributes() { return Map.of(); }
+    @Override public String getName() { return ""; }
 
     public enum RoleType {
         USER("ROLE_USER");
